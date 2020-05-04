911ee66cf2cd3b2e4702952dba858304711e324d
efrain <efarinva98@gmail.com>
2020-05-04 19:11:07 -0400

    graphs mvp

--- adv.py
+++ adv.py
@@ -28,7 +28,70 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+# 
+def next_path(starting_room, all_rooms=set()):
+    visited = set()
 
+    for room in all_rooms:  
+        visited.add(room) 
+        path = [] 
+        opposite = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'} # directions 
+
+        def add_to_path(r, back_to=None):
+            visited.add(r)
+            exits = r.get_exits() 
+            for direction in exits:
+                # print(direction)
+                if r.get_room_in_direction(direction) not in visited: 
+                    path.append(direction) 
+                    add_to_path(r.get_room_in_direction(direction), opposite[direction]) 
+
+            if back_to: #  is None
+                path.append(back_to) 
+
+        add_to_path(starting_room) 
+
+def create_path(starting_room, visited=set()):
+    path = []
+    opposite = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'} 
+
+    def add_to_path(room, back_to=None):
+        visited.add(room)  
+        exits = room.get_exits() 
+        path_length = {} 
+        traverse_order = [] 
+        # print("show traverse order as empty", traverse_order)
+
+        for direction in exits:
+            # print(direction)
+            path_length[direction] = len(next_path(room.get_room_in_direction(direction), visited))
+
+
+        for key, value in sorted(path_length.items(), key=lambda val: val[1]): 
+            print("this is the len of our path", key, value)
+            print(key, value) 
+            traverse_order.append(key) 
+
+            print(" my traverse order list", traverse_order)
+
+        for direction in traverse_order: 
+            # print(direction)
+            if room.get_room_in_direction(direction) not in visited: 
+                path.append(direction) 
+                add_to_path(room.get_room_in_direction(direction), opposite[direction]) 
+
+        if len(visited) == len(world.rooms): 
+            return 
+        elif back_to:
+            path.append(back_to) 
+
+    add_to_path(starting_room) 
+    return path 
+
+
+traversal_path = create_path(world.starting_room) 
+
+# 
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
